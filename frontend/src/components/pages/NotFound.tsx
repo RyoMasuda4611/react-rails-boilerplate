13cff9ff0bc732be157685681cbd531825738494
Ryo Masuda <mrra4611@gmail.com>
2021-01-28 20:49:43 +0900

    Added default NotFound Page

---
+++ frontend/src/components/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFoundPage = (): JSX.Element => (
+  <div className="clearfix">
+    <h1 className="float-left display-3 mr-4">404</h1>
+    <h4 className="pt-3">Oops! You’re lost.</h4>
+    <p className="text-muted float-left">
+      The page you are looking for was not found.
+    </p>
+  </div>
+)
+
+export default NotFoundPage
